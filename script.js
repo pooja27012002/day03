0b411a0405b3003f630a551ef9a58b8a79ff2dbf
dac <dac@dypm.org>
2024-04-13 15:43:19 +0530

    Changes

---
+++ script.js
@@ -0,0 +1,8 @@
+[
+    {'id':01, 'title':'gerbera',
+    'description':'Wedding','unitprice':5,'quantity':400,'likes':50000 },
+    {'id':02, 'title':'Rose',
+    'description':'Wedding plan','unitprice':5,'quantity':500,'likes':50050 },
+    {'id':03, 'title':'Lotus',
+    'description':'BJP','unitprice':8,'quantity':480,'likes':50000 },
+]
